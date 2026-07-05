b6c09d68617eb3047eef6f2f25e8cb49a5561508
xilda622 <3695524991@qq.com>
2026-07-05 13:33:47 +0000

    Fix JVM target mismatch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,22 @@ android {
         versionName = "1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
         }
     }
 }
+
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
