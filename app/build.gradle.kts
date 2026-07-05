d96c1ffe1e468d164d5eb9b6b7229690d7c21a08
xilda622 <3695524991@qq.com>
2026-07-05 13:18:15 +0000

    Add repositories to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,13 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
+
+repositories {
+    google()
+    mavenCentral()
+}
+
